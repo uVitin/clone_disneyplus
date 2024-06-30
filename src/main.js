2f4b1b338f586bb74c74a6a4f16c5e2f90364f6c
Albert Vitor <albert.vitor@icloud.com>
2024-06-30 14:06:29 -0300

    Criação do header!

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+            ocultaElementoDoHeader();
+        } else {
+            exibiElementosDoHeader();
+        }
+    })
+
+
+    //Sessão de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,10 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    //Sessão FAQ, Accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+function ocultaElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibiElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
